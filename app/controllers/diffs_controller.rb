c9c13504d7090e30d3c6c2bcfc163c41fc9edb41
liufengyun <liufengyunchina@gmail.com>
2012-06-03 19:42:06 +0800

    try fix parsing invalid value

--- app/controllers/diffs_controller.rb
+++ app/controllers/diffs_controller.rb
@@ -2,11 +2,11 @@ class DiffsController < ApplicationController
   # GET /diff obj1=:obj1 obj2=:obj2 type=yaml|json
   def diff
     if params[:type] == "yaml"
-      @obj1 = YAML::load params[:obj1] rescue -1
-      @obj2 = YAML::load params[:obj2] rescue -1
+      @obj1 = (YAML::load(params[:obj1]) rescue -1)
+      @obj2 = (YAML::load(params[:obj2]) rescue -1)
     elsif params[:type] == "json"
-      @obj1 = JSON.parse params[:obj1] rescue -1
-      @obj2 = JSON.parse params[:obj2] rescue -1
+      @obj1 = (JSON.parse(params[:obj1]) rescue -1)
+      @obj2 = (JSON.parse(params[:obj2]) rescue -1)
     else
       return render :text => "error: type must be specified. type can be yaml or json."
     end

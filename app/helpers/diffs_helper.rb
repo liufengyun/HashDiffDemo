685bf0895f752cb50bda098b8ae904601d4d5a12
liufengyun <liufengyunchina@gmail.com>
2012-06-24 18:18:38 +0800

    update demo to v0.0.4

--- app/helpers/diffs_helper.rb
+++ app/helpers/diffs_helper.rb
@@ -2,22 +2,8 @@ module DiffsHelper
   def change_html(change)
     template = %q[<span class="type">%s</span><span class="property">%s</span><span class="value">%s</span>]
     html = change[2].to_s.split('')
-    if change[3]
-      changes = Diff::LCS.diff(change[2].to_s, change[3].to_s).flatten
-
-      adds = 0
-      deletes = 0
-      changes.each do |change|
-        if change.action == '+'
-          adds += 1
-          html.insert(change.position + deletes, %q[<span class="insert">%s</span>] % change.element)
-        elsif change.action == '-'
-          deletes += 1
-          html[change.position + adds] = (%q[<span class="delete">%s</span>] % change.element)
-        end
-      end
-
-      (template % [change[0], change[1], html.flatten.join]).html_safe
+    if change[0] == '~'
+      (template % [change[0], change[1], "#{change[2].to_json}    ->    #{change[3].to_json}"]).html_safe
     else
       (template % [change[0], change[1], change[2].to_s]).html_safe
     end
